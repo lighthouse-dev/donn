2745eca052e4a051f1281d10c579ed29ba47838d
JuseungLee <juseung@fusic.co.jp>
2018-08-24 20:58:00 +0900

    :sparkles: ログイン画面をそれっぽく修正

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
-  ngOnInit() {
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
   }
 
+  createForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required ],
+      password: ['', Validators.required ]
+    });
+  }
+
+  login(value) {
+    this.authService.login(value)
+    .then(res => {
+      console.log(res);
+      this.router.navigate(['/user']);
+    }, err => {
+      console.log(err);
+      this.errorMessage = err.message;
+    });
+  }
 }
