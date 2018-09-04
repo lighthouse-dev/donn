017961a52f84e748bb8c7a518a6dbb3f65cae446
JuseungLee <juseung@fusic.co.jp>
2018-09-05 01:13:15 +0900

    :sparkles: material-moment-adapterを導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { routes } from './app-routing.module';
 // Module
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
 // Firebase Module
 import { AngularFireModule } from 'angularfire2';
@@ -64,7 +66,10 @@ import { SpendPrivateListComponent } from './components/spend/list/private/spend
     UserService,
     UserResolver,
     AuthGuard,
-    SpendService
+    SpendService,
+    { provide: MAT_DATE_LOCALE, useValue: 'ja-JP' },
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ],
   bootstrap: [AppComponent]
 })
