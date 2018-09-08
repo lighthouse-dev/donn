304b873af56f96f96b77af7789241e4d36368b95
JuseungLee <juseung@fusic.co.jp>
2018-09-08 16:30:19 +0900

    :sparkles: メニューから「共有履歴」「個人履歴」を消す

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 // Component
 import { LoginComponent } from './components/login/login.component';
 import { SpendComponent } from './components/spend/add/spend.component';
-import { SpendPublicListComponent } from './components/spend/list/public/spend-public-list.component';
-import { SpendPrivateListComponent } from './components/spend/list/private/spend-private-list.component';
+import { SpendListComponent } from './components/spend/list/spend-list.component';
 
 // Auth
 import { UserResolver } from './core/user.resolver';
@@ -16,8 +15,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'spend', component: SpendComponent,  resolve: { data: UserResolver}},
-  { path: 'spend-public-list', component: SpendPublicListComponent,  resolve: { data: UserResolver}},
-  { path: 'spend-private-list', component: SpendPrivateListComponent,  resolve: { data: UserResolver}}
+  { path: 'spend-list', component: SpendListComponent,  resolve: { data: UserResolver}}
 ];
 
 @NgModule({
