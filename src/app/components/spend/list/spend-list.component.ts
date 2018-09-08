727616c2da22c8f52f06ab9b0255f186a070bb2c
JuseungLee <juseung@fusic.co.jp>
2018-09-08 16:29:27 +0900

    :sparkles: 履歴を１つの画面に修正、Tab化

---
+++ src/app/components/spend/list/spend-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/filter';
+
+
+@Component({
+  selector: 'app-spend-list',
+  templateUrl: './spend-list.component.html',
+  styleUrls: ['./spend-list.component.scss']
+})
+export class SpendListComponent implements OnInit {
+  isPublic: Boolean = false;
+  publicTapNum: Number  = 0;
+  privateTapNum: Number = 1;
+  selectedTab = this.privateTapNum;
+
+  constructor(private route: ActivatedRoute) {
+    // 初期表示TABを指定
+    this.route.queryParams
+      .filter(params => params.isPublic)
+      .subscribe(params => {
+        this.selectedTab = params.isPublic === 'true' ? this.publicTapNum : this.privateTapNum;
+      });
+  }
+
+  ngOnInit() {
+  }
+}
