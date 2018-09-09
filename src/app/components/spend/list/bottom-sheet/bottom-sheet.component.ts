f44788d9be4cbf41bd67161675f8c2c63528b1b6
JuseungLee <juseung@fusic.co.jp>
2018-09-10 00:31:44 +0900

    :sparkles: 削除機能実装

---
+++ src/app/components/spend/list/bottom-sheet/bottom-sheet.component.ts
@@ -0,0 +1,51 @@
+import { Component, Inject } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material';
+import { Spend } from '../../../../model/spend';
+import { SpendService } from '../../../../service/spend.service';
+import { DeleteSpendDialogComponent } from './dialog/delete-spend-dialog.component';
+
+@Component({
+  selector: 'app-bottom-sheet',
+  templateUrl: './bottom-sheet.component.html',
+  styleUrls: ['./bottom-sheet.component.scss']
+})
+export class BottomSheetComponent {
+  spendData: Spend = null;
+
+  constructor(
+    private bottomSheetRef: MatBottomSheetRef<BottomSheetComponent>,
+    public dialog: MatDialog,
+    private spendService: SpendService,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: any
+  ) {
+    this.spendData = data;
+  }
+
+  deleteAlertDialog() {
+    // 削除確認のDialogを表示
+    const dialogRef = this.dialog.open(DeleteSpendDialogComponent, {
+      data: {spend: this.spendData}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.deleteSpend();
+      }
+      // bottomSheetを閉じる
+      this.bottomSheetRef.dismiss();
+    });
+  }
+
+  // 削除処理
+  deleteSpend() {
+    this.spendService.deleteSpend(
+      this.spendData['spend']['$key'],
+      this.spendData['isPublic']
+    ).then(ref => {
+      // todo:: 成功したら、メッセージを表示する (MatSnackBarModule)
+    });
+  }
+
+}
