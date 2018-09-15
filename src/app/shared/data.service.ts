67795bd66dedab39caa0b4a5125b9965d1f389e8
JuseungLee <juseung@fusic.co.jp>
2018-09-15 20:55:27 +0900

    :recycle: 定数をきれいに修正

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -15,33 +15,33 @@ const PRIVATE_CATEGORY_BEAUTY       = 22;
 const PRIVATE_CATEGORY_ETC          = 29;
 
 export const PUBLIC_CATEGORY_LIST = {
-  10: '食費',
-  11: '日用品',
-  12: '交際',
-  13: '旅行・本・趣味',
-  14: 'お住まい・水道・光熱・通信',
-  19: 'その他'
+  [PUBLIC_CATEGORY_FOOD]: '食費',
+  [PUBLIC_CATEGORY_DAILY_NECESSITIES]: '日用品',
+  [PUBLIC_CATEGORY_RELATIINSHIP]: '交際',
+  [PUBLIC_CATEGORY_HOBBY]: '旅行・本・趣味',
+  [PUBLIC_CATEGORY_HOUSE]: 'お住まい・水道・光熱・通信',
+  [PUBLIC_CATEGORY_ETC]: 'その他'
 };
 
 export const PRIVATE_CATEGORY_LIST = {
-  20: '食費',
-  21: '交際',
-  22: '服・美容',
-  29: 'その他'
+  [PRIVATE_CATEGORY_FOOD]: '食費',
+  [PRIVATE_CATEGORY_RELATIINSHIP]: '交際',
+  [PRIVATE_CATEGORY_BEAUTY]: '服・美容',
+  [PRIVATE_CATEGORY_ETC]: 'その他'
 };
 
 export const publicCategory: Category[] = [
-  { value: PUBLIC_CATEGORY_FOOD, viewValue: '食費' },
-  { value: PUBLIC_CATEGORY_DAILY_NECESSITIES, viewValue: '日用品' },
-  { value: PUBLIC_CATEGORY_RELATIINSHIP, viewValue: '交際' },
-  { value: PUBLIC_CATEGORY_HOBBY, viewValue: '旅行・本・趣味' },
-  { value: PUBLIC_CATEGORY_HOUSE, viewValue: 'お住まい・水道・光熱・通信' },
-  { value: PUBLIC_CATEGORY_ETC, viewValue: 'その他' },
+  { value: PUBLIC_CATEGORY_FOOD,              viewValue: PUBLIC_CATEGORY_LIST[PUBLIC_CATEGORY_FOOD] },
+  { value: PUBLIC_CATEGORY_DAILY_NECESSITIES, viewValue: PUBLIC_CATEGORY_LIST[PUBLIC_CATEGORY_DAILY_NECESSITIES] },
+  { value: PUBLIC_CATEGORY_RELATIINSHIP,      viewValue: PUBLIC_CATEGORY_LIST[PUBLIC_CATEGORY_RELATIINSHIP] },
+  { value: PUBLIC_CATEGORY_HOBBY,             viewValue: PUBLIC_CATEGORY_LIST[PUBLIC_CATEGORY_HOBBY] },
+  { value: PUBLIC_CATEGORY_HOUSE,             viewValue: PUBLIC_CATEGORY_LIST[PUBLIC_CATEGORY_HOUSE] },
+  { value: PUBLIC_CATEGORY_ETC,               viewValue: PUBLIC_CATEGORY_LIST[PUBLIC_CATEGORY_ETC] },
 ];
 
 export const privateCategory: Category[] = [
-  { value: PRIVATE_CATEGORY_FOOD, viewValue: '食費' },
-  { value: PRIVATE_CATEGORY_RELATIINSHIP, viewValue: '交際' },
-  { value: PRIVATE_CATEGORY_BEAUTY, viewValue: '服・美容' },
-  { value: PRIVATE_CATEGORY_ETC, viewValue: 'その他' },
+  { value: PRIVATE_CATEGORY_FOOD,         viewValue: PRIVATE_CATEGORY_LIST[PRIVATE_CATEGORY_FOOD] },
+  { value: PRIVATE_CATEGORY_RELATIINSHIP, viewValue: PRIVATE_CATEGORY_LIST[PRIVATE_CATEGORY_RELATIINSHIP] },
+  { value: PRIVATE_CATEGORY_BEAUTY,       viewValue: PRIVATE_CATEGORY_LIST[PRIVATE_CATEGORY_BEAUTY] },
+  { value: PRIVATE_CATEGORY_ETC,          viewValue: PRIVATE_CATEGORY_LIST[PRIVATE_CATEGORY_ETC] },
 ];
