f8ea376ebdfd770ea0509d5d1cf30d6b16014470
lighthouse-dev <lighthouse2.dev@gmail.com>
2019-09-07 20:09:38 +0900

    :construction: 固定支出一覧作成（途中まで）

--- src/app/components/fixed-spend/list/fixed-spend-list.component.ts
+++ src/app/components/fixed-spend/list/fixed-spend-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, OnInit } from '@angular/core';
+import { Spend } from '../../../model/spend';
+import { SpendService } from '../../../service/spend.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { AddFixedSpendComponent } from '../add/add-fixed-spend.component';
+import store from '../../../store/spendType';
 
 @Component({
   selector: 'app-fixed-spend-list',
@@ -6,10 +13,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fixed-spend-list.component.scss']
 })
 export class FixedSpendListComponent implements OnInit {
+  fixedSpendList: Spend[];
+  spendTempList: Spend[];
+  totalAmount: Number   = 0;
+  storeObj = store;
 
-  constructor() { }
+  dataSource: MatTableDataSource<Spend>;
+  displayedColumns: string[] = ['createDate', 'amount'];
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+  constructor(
+    private spendService: SpendService,
+    public dialog: MatDialog
+  ) {}
+
+  async ngOnInit() {
+    await this.getFixedSpendList().then( () => {
+      this.dataSource = new MatTableDataSource(this.fixedSpendList);
+      this.dataSource.sort = this.sort;
+    });
+    this.storeObj.setShowFooter();
   }
 
+  getFixedSpendList() {
+    return new Promise( resolve => {
+      const data = this.spendService.getSpendList();
+      data.snapshotChanges().subscribe(item => {
+        // 初期化
+        this.spendTempList  = [];
+        this.totalAmount    = 0;
+
+        item.forEach(element => {
+          const json = element.payload.toJSON();
+          json['$key'] = element.key;
+          this.spendTempList.push(json as Spend);
+
+          // 合計計算
+          this.totalAmount += json['amount'];
+        });
+
+        this.fixedSpendList = this.spendTempList;
+        resolve();
+      });
+    });
+  }
+
+  openAddDialog() {
+    this.dialog.open(AddFixedSpendComponent, {
+      width: '75%',
+      // data: this.spendData
+    });
+  }
 }
